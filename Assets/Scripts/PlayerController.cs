d7a75ce96644d52fb757feada5233ed224bc2775
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-06-14 23:53:43 +0900

    add file

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float distance = 1f;
+    [SerializeField] private float speed = 1f;
+    private Vector2 move;
+    Sequence moveSeq;
+
+    public GameObject playerManager;
+    PlayerManager pManager;
+
+    void Start()
+    {
+        move = this.transform.position;
+        pManager = playerManager.GetComponent<PlayerManager>();
+    }
+
+    public void MoveUp()
+    {
+        move.y += distance;
+        moveSeq = MoveSequence();
+        moveSeq.Play();
+    }
+    public void MoveDown()
+    {
+        move.y -= distance;
+        moveSeq = MoveSequence();
+        moveSeq.Play();
+    }
+    public void MoveRight()
+    {
+        move.x += distance;
+        moveSeq = MoveSequence();
+        moveSeq.Play();
+    }
+    public void MoveLeft()
+    {
+        move.x -= distance;
+        moveSeq = MoveSequence();
+        moveSeq.Play();
+    }
+    public Sequence MoveSequence()
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScale(new Vector2(1.2f, 1.2f), .2f))
+                .Append(transform.DOLocalMove(move, speed))
+                .Join(transform.DOScale(new Vector2(.5f, .5f), .2f))
+                .Append(transform.DOScale(new Vector2(1.2f, 1.2f), .2f))
+                .Append(transform.DOScale(new Vector2(1f, 1f), .2f));
+        return sequence;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "red")
+        {
+            pManager.AddExp(Random.Range(50, 100), 10);
+        }
+        if(other.gameObject.tag == "blue")
+        {
+            pManager.AddHP(20);
+        }
+        if(other.gameObject.tag == "gold")
+        {
+            pManager.AddMoney(500);
+        }
+        if(other.gameObject.tag == "silver")
+        {
+            pManager.AddPower(10, 100);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    //UI
+    public Slider hpSlider;
+    public Slider expSlider;
+    public Text pNameText;
+    public Text levelText;
+    public Text hpText;
+    public Text moneyText;
+    public Text powerText;
+
+    //Status
+    [SerializeField] public string pName;
+    [SerializeField] public int level;
+    [SerializeField] public int maxHP;
+    public int hp;
+    [SerializeField] public int maxExp;
+    public int exp;
+    [SerializeField] public int money;
+    [SerializeField] public int power;
+
+    void Start()
+    {
+        //スライダーのセット
+        hp = maxHP;
+        hpSlider.maxValue = maxHP;
+        exp = 0;
+        expSlider.maxValue = maxExp;
+    }
+
+    void Update()
+    {
+        //テキストのセット・スライダーの更新
+        pNameText.text = pName;
+        levelText.text = "Lv." + level;
+        hpText.text = hp + "/" + maxHP;
+        hpSlider.value = hp;
+        expSlider.value = exp;
+        moneyText.text = "G:" + money;
+        powerText.text = "POW:" + power;
+    }
+
+    public void AddLevel()
+    {
+    }
+    public void AddHP(int cure)
+    {
+        if(hp + cure > maxHP)
+        {
+            int diff = (hp + cure) - maxHP;
+            hp += cure - diff;
+            return;
+        }
+        hp += cure;
+    }
+    public void AddExp(int ex, int damage)
+    {
+        if(exp + ex >= maxExp)
+        {
+            int diff = (exp + ex) - maxExp;
+            exp = 0;
+            level++;
+            AddExp(diff, damage);
+        }else
+        {
+            exp += ex;
+            hp -= damage;
+        }
+    }
+    public void AddMoney(int gold)
+    {
+        money += gold;
+    }
+    public void AddPower(int pow, int gold)
+    {
+        power += pow;
+        money -= gold;
+    }
+}
