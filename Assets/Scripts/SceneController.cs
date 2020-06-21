c7fac3a7906b553be13495befa90644afc6dbced
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-06-22 03:28:21 +0900

    Add TitleScene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController sceneController;
+    public string[] scenes = {
+        "TitleScene",
+        "SampleScene"
+    };
+
+    void Awake()
+    {
+        if(sceneController == null)
+        {
+            sceneController = this;
+            DontDestroyOnLoad(gameObject);
+        }else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TitleToSample()
+    {
+        SceneManager.LoadScene(scenes[1]);
+    }
+
+}
