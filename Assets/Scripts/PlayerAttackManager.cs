2ef1fb241ddc7313601d789c88467327c8ac3c1b
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-06-24 23:39:10 +0900

    Add AttackManager

---
+++ Assets/Scripts/PlayerAttackManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttackManager : MonoBehaviour
+{
+    public enum Skill {
+        Punch,
+        Kick,
+        Slash
+    };
+    [SerializeField] public Dictionary<Skill, int> skillDictionary = new Dictionary<Skill, int>();
+    void Start()
+    {
+        skillDictionary[Skill.Punch] = 2;
+        foreach(var skill in skillDictionary)
+        {
+            Debug.Log(skill.Key + ":" + GetNum(skill.Key));
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public int GetNum(Skill key)
+    {
+        return skillDictionary[key];
+    }
+}
