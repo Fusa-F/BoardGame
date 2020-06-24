95598dfefdbc4e8b4a42aba951e76c1ad858c633
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-06-24 23:59:47 +0900

    Fixed AttackManager

--- Assets/Scripts/PlayerAttackManager.cs
+++ Assets/Scripts/PlayerAttackManager.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class PlayerAttackManager : MonoBehaviour
 {
-    [SerializeField] public Dictionary<string, Skill> skill = new Dictionary<string, Skill>();
+    [SerializeField] public Dictionary<string, SkillBase> skill = new Dictionary<string, SkillBase>();
     void Start()
     {
-        skill["パンチ"] = new Skill(10, 1, "こぶしで殴りつける。");
-        skill["キック"] = new Skill(15, 1, "思いっきり蹴りとばす。");
-        skill["スラッシュ"] = new Skill(30, 2, "鉄のつるぎで斬りつける。");
+        skill["パンチ"] = new SkillBase(10, 1, "こぶしで殴りつける。");
+        skill["キック"] = new SkillBase(15, 1, "思いっきり蹴りとばす。");
+        skill["スラッシュ"] = new SkillBase(30, 2, "鉄のつるぎで斬りつける。");
 
         foreach (var key in skill.Keys)
         {
@@ -18,12 +18,12 @@ public class PlayerAttackManager : MonoBehaviour
         }
     }
 
-    private class Skill {
+    private class SkillBase {
         public int damage;
         public int rarity;
         public string caption;
 
-        public Skill(int damage, int rarity, string caption)
+        public SkillBase(int damage, int rarity, string caption)
         {
             this.damage = damage;
             this.rarity = rarity;
