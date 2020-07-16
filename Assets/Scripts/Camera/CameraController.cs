198c3f2ab35d3d5c1bd549cdc998cc7b0e79cdf9
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-07-16 12:14:09 +0900

    Add CameraController

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraController : MonoBehaviour
+{
+    public float offsetX;
+    public float offsetY;
+    public GameObject target;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    public void SetTarget(GameObject obj)
+    {
+        offsetX = transform.position.x - obj.transform.position.x;
+        offsetY = transform.position.y - obj.transform.position.y;
+        target = obj;
+    }
+    public void MoveCamera()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10);
+    }
+}
---
+++ Assets/Scripts/Map/TileMapGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapGenerator : MonoBehaviour
+{
+    //mapサイズ
+    public int mapWidth = 10;
+    public int mapHeight = 10;
+    public int[,] map;
+
+    //tile色
+    public int white = 0;
+    public int red = 1;
+
+    //tileオブジェクト取得
+    [SerializeField] public TileBase tile;
+    [SerializeField] public GameObject tilemapObj;
+    Tilemap tilemap;
+
+    void Start()
+    {
+        tilemap = tilemapObj.GetComponent<Tilemap>();
+
+        SetTileMapData();
+        CreateMap();
+    }
+
+    /// <summary>
+    /// mapの2次元配列初期化
+    /// </summary>
+    private void SetTileMapData()
+    {
+        map = new int[mapHeight, mapWidth];
+        for(int i = 0; i < mapHeight; i++)
+        {
+            for(int j = 0; j < mapWidth; j++)
+            {
+                map[i, j] = white;
+            }
+        }
+    }
+
+    /// <summary>
+    /// mapdataをもとにタイル配置
+    /// </summary>
+    private void CreateMap()
+    {
+        for(int i = 0; i < mapHeight; i++)
+        {
+            for(int j = 0; j < mapWidth; j++)
+            {
+                if(map[i, j] == white)
+                {
+                    Vector3Int pos = new Vector3Int(j, i, 0);
+                    tilemap.SetTile(pos, tile);
+                }
+            }
+        }
+    }
+}
