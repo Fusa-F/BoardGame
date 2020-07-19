caee042b8e82c435f7eb7b76e5741c9430dde61e
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-07-20 03:01:45 +0900

    Update PlayerStatus-move

--- Assets/Scripts/Character/Player/PlayerStatusUIManager.cs
+++ Assets/Scripts/Character/Player/PlayerStatusUIManager.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PlayerStatusUIManager : MonoBehaviour
 {
+    RectTransform rect;
+   void Awake()
+   {
+        rect = this.gameObject.GetComponent<RectTransform>();
+        rect.DOLocalMoveY(400f, 0f).SetRelative();
+   } 
     void Start()
     {
-        
+        // rect = this.gameObject.GetComponent<RectTransform>();
+        rect.DOLocalMoveY(-400f, 2f).SetRelative();
     }
 
     void Update()
