4e0370efcfbe0fbff7fc0a079b0315bd71ac1610
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-07-23 00:51:36 +0900

    Add SkillModel

---
+++ Assets/Scripts/Character/Player/PlayerSkillManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkillManager : MonoBehaviour
+{
+    //プレイヤーの持つスキルのリスト
+    public List<GameObject> skillList = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            SetSkillList();
+        }
+    }
+
+    //test
+    public void SetSkillList()
+    {
+        skillList.Add((GameObject)Resources.Load("Prefab/Skill/Skill_00"));
+        GameObject obj = Instantiate(skillList[0], transform.position, Quaternion.identity);
+        obj.transform.parent = transform;
+    }
+}
